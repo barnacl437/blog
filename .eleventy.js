b2e528f750767d816bfde00cb51fc053790bce23
barnacl437 <barnacl437@disroot.org>
2025-04-16 00:51:03 +0700

    some fixes

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,24 @@
 module.exports = function(eleventyConfig) {
-  
-    eleventyConfig.addFilter('readableDate', (dateObj, options) => {
-      if (!dateObj) {
-        return '';
-      }
-  
-      // Default options if none are provided
-      const defaultOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-      const combinedOptions = { ...defaultOptions, ...options };
-  
-      return dateObj.toLocaleDateString(undefined, combinedOptions);
-    });
-  
+  // Add 'readableDate' filter
+  eleventyConfig.addFilter('readableDate', (dateObj, options) => {
+    if (!dateObj) {
+      return '';
+    }
 
-  };
+    // Default options if none are provided
+    const defaultOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    const combinedOptions = { ...defaultOptions, ...options };
+
+    return dateObj.toLocaleDateString(undefined, combinedOptions);
+  });
+
+  // Add 'sortedPosts' collection (inside module.exports!)
+  eleventyConfig.addCollection("sortedPosts", function(collectionApi) {
+    return collectionApi.getFilteredByTag("post")
+      .sort((a, b) => {
+        const dateA = a.date || a.inputPath.stats.birthtime;
+        const dateB = b.date || b.inputPath.stats.birthtime;
+        return dateB - dateA; // Newest first
+      });
+  });
+}; // <-- Closing brace for module.exports
